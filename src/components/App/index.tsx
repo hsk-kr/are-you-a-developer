6cb72007b2983949e4f6008341bc3f1beae7b584
hsk_kr <hsk.coder@gmail.com>
2020-03-18 00:43:20 +0900

    Delete unnecessary files

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.scss";
+import Main from "../Main";
+
+const App = () => {
+  return <Main />;
+};
+
+export default App;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = (): React.ReactElement => {
+  return (
+    <>
+      <div>title</div>
+      <div>start</div>
+      <div>history</div>
+      <div>setting</div>
+    </>
+  );
+};
+
+export default Main;
