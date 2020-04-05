d6d75474fd6e2507f2dbb695f442c7506ee107b2
hsk_kr <hsk.coder@gmail.com>
2020-04-05 15:42:49 +0900

    Shuffle Questions

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,96 +1,109 @@
-import React, { useEffect, useState, useContext, useCallback } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { QuestionsContext, questions } from "../../contexts/QuestionsContext";
 import { LanguageContext } from "../../contexts/LanguageContext";
 import "./styles.scss";
 import { Redirect } from "react-router-dom";
 
+/**
+ * Returns shuffled questions
+ */
+const generateRandNumList = (srtN: number, endN: number): number[] => {
+  const numSequence: number[] = Array.from(
+    new Array(endN - srtN + 1),
+    (_, i) => i + srtN
+  );
+  const randNumList: number[] = [];
+
+  while (numSequence.length > 0) {
+    const rIdx = Math.floor(Math.random() * numSequence.length);
+    randNumList.push(numSequence.splice(rIdx, 1)[0]);
+  }
+
+  return randNumList;
+};
+
 const Game = () => {
-    const { changeIdxRandomly, currentQuestionIdx } = useContext(
-        QuestionsContext
-    );
-    const { lang } = useContext(LanguageContext);
-    const [progressWidthPercentage, setProgressWidthPercentage] = useState(100);
-    const [isGameEnd, gameEnd] = useState(false);
-    const [correctAnswerNum, setCorrectAnswerNum] = useState(0);
-    const [totalQuestionNum, setTotalQuestionNum] = useState(0);
-    const [questionObj, setQuestionObj] = useState(null);
+  const { changeIdxRandomly, currentQuestionIdx } = useContext(
+    QuestionsContext
+  );
+  const { lang } = useContext(LanguageContext);
+  const [progressWidthPercentage, setProgressWidthPercentage] = useState(100);
+  const [isGameEnd, gameEnd] = useState(false);
+  const [correctAnswerNum, setCorrectAnswerNum] = useState(0);
+  const [totalQuestionNum, setTotalQuestionNum] = useState(0);
+  const [questionObj, setQuestionObj] = useState(null);
+  const [randNumList, setRandNumList] = useState([0, 1, 2, 3]);
 
-    const checkAnswer = awNum => {
-        setTotalQuestionNum(totalQuestionNum + 1);
+  const checkAnswer = awNum => {
+    setTotalQuestionNum(totalQuestionNum + 1);
 
-        if (awNum === questions[lang][currentQuestionIdx].answerIdx) {
-            setCorrectAnswerNum(correctAnswerNum + 1);
-        }
-
-        changeIdxRandomly(lang);
-    };
-
-    // Initalize Effect
-    useEffect(() => {
-        changeIdxRandomly(lang);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    // change a question when question idx changed
-    useEffect(() => {
-        setQuestionObj(questions[lang][currentQuestionIdx]);
-    }, [currentQuestionIdx, lang]);
-
-    // change progress and process to finish the game
-    useEffect(() => {
-        if (progressWidthPercentage > 0) {
-            setTimeout(() => {
-                setProgressWidthPercentage(progressWidthPercentage - 0.1);
-            }, 10);
-        } else {
-            // gameEnd(true);
-        }
-    }, [progressWidthPercentage]);
-
-    if (isGameEnd) {
-        return <Redirect to={"game-result"} />;
+    if (awNum === questions[lang][currentQuestionIdx].answerIdx) {
+      setCorrectAnswerNum(correctAnswerNum + 1);
     }
 
-    if (questionObj === null) {
-        return null;
-    }
+    changeIdxRandomly(lang);
+  };
 
-    return (
-        <div className="game-container">
-            <div className="time-container">
-                <div className="time-icon-box">
-                    <img
-                        src="/images/game/game-clock.png"
-                        alt="Clock Icon"
-                        className="clock-icon animated infinite swing delay-1s"
-                    />
-                </div>
-                <div className="progress-box">
-                    <div
-                        className="progress-bar"
-                        style={{ width: `${progressWidthPercentage}%` }}
-                    ></div>
-                </div>
-            </div>
-            <div className="question-box">
-                <pre>{questionObj.question}</pre>
-            </div>
-            <div className="answers-box">
-                <div className="answer" onClick={() => checkAnswer(0)}>
-                    {questionObj.answerList[0]}
-                </div>
-                <div className="answer" onClick={() => checkAnswer(1)}>
-                    {questionObj.answerList[1]}
-                </div>
-                <div className="answer" onClick={() => checkAnswer(2)}>
-                    {questionObj.answerList[2]}
-                </div>
-                <div className="answer" onClick={() => checkAnswer(3)}>
-                    {questionObj.answerList[3]}
-                </div>
-            </div>
+  // Initalize Effect
+  useEffect(() => {
+    changeIdxRandomly(lang);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // change a question when question idx changed
+  useEffect(() => {
+    setRandNumList(generateRandNumList(0, 3));
+    setQuestionObj(questions[lang][currentQuestionIdx]);
+  }, [currentQuestionIdx, lang]);
+
+  // change progress and process to finish the game
+  useEffect(() => {
+    if (progressWidthPercentage > 0) {
+      setTimeout(() => {
+        setProgressWidthPercentage(progressWidthPercentage - 0.1);
+      }, 10);
+    } else {
+      // gameEnd(true);
+    }
+  }, [progressWidthPercentage]);
+
+  if (isGameEnd) {
+    return <Redirect to={"game-result"} />;
+  }
+
+  if (questionObj === null) {
+    return null;
+  }
+
+  return (
+    <div className="game-container">
+      <div className="time-container">
+        <div className="time-icon-box">
+          <img
+            src="/images/game/game-clock.png"
+            alt="Clock Icon"
+            className="clock-icon animated infinite swing delay-1s"
+          />
         </div>
-    );
+        <div className="progress-box">
+          <div
+            className="progress-bar"
+            style={{ width: `${progressWidthPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+      <div className="question-box">
+        <pre>{questionObj.question}</pre>
+      </div>
+      <div className="answers-box">
+        {randNumList.map((rIdx, i) => (
+          <div key={i} className="answer" onClick={() => checkAnswer(rIdx)}>
+            {questionObj.answerList[rIdx]}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Game;
